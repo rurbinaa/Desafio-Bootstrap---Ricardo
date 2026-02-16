c36942aca3eeb20a571b30e542c0de4e6b627fe2
rurbinaa <ricardour2014@gmail.com>
2026-02-16 13:53:21 -0600

    implement list view rendering

--- logic.js
+++ logic.js
@@ -1,34 +1,52 @@
-    const materias = [
-        { codigo: "0413", nombre: "PROGRAMACIÓN WEB", grupo: "Gpo1", dia: "MARTES", horaInicio: 10, horaFin: 12, aula: "E201" },
-        { codigo: "0413", nombre: "PROGRAMACIÓN WEB", grupo: "Gpo1", dia: "JUEVES", horaInicio: 15, horaFin: 17, aula: "E201" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo3", dia: "LUNES", horaInicio: 8, horaFin: 10, aula: "D104" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo3", dia: "MIÉRCOLES", horaInicio: 8, horaFin: 10, aula: "D104" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo4", dia: "LUNES", horaInicio: 10, horaFin: 12, aula: "E201" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo4", dia: "JUEVES", horaInicio: 8, horaFin: 10, aula: "E201" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo7", dia: "LUNES", horaInicio: 15, horaFin: 17, aula: "D104" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo7", dia: "JUEVES", horaInicio: 13, horaFin: 15, aula: "D104" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo8", dia: "MARTES", horaInicio: 15, horaFin: 17, aula: "D104" },
-        { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo8", dia: "MIÉRCOLES", horaInicio: 13, horaFin: 15, aula: "D104" }
-    ];
+const materias = [
+    { codigo: "0413", nombre: "PROGRAMACIÓN WEB", grupo: "Gpo1", dia: "MARTES", horaInicio: 10, horaFin: 12, aula: "E201" },
+    { codigo: "0413", nombre: "PROGRAMACIÓN WEB", grupo: "Gpo1", dia: "JUEVES", horaInicio: 15, horaFin: 17, aula: "E201" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo3", dia: "LUNES", horaInicio: 8, horaFin: 10, aula: "D104" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo3", dia: "MIÉRCOLES", horaInicio: 8, horaFin: 10, aula: "D104" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo4", dia: "LUNES", horaInicio: 10, horaFin: 12, aula: "E201" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo4", dia: "JUEVES", horaInicio: 8, horaFin: 10, aula: "E201" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo7", dia: "LUNES", horaInicio: 15, horaFin: 17, aula: "D104" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo7", dia: "JUEVES", horaInicio: 13, horaFin: 15, aula: "D104" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo8", dia: "MARTES", horaInicio: 15, horaFin: 17, aula: "D104" },
+    { codigo: "0402", nombre: "INTRO. INGENIERÍA", grupo: "Gpo8", dia: "MIÉRCOLES", horaInicio: 13, horaFin: 15, aula: "D104" }
+];
 
-    const diasSemana = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES"];
-    const appContainer = document.getElementById('app');
-    const btnLista = document.getElementById('btnLista');
-    const btnCalendario = document.getElementById('btnCalendario');
+const diasSemana = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES"];
+const appContainer = document.getElementById('app');
+const btnLista = document.getElementById('btnLista');
+const btnCalendario = document.getElementById('btnCalendario');
 
-    renderLista();
+renderLista();
 
-    function cambiarVista(vista) {
-        const vLista = document.getElementById('vistaLista');
-        const vCal = document.getElementById('vistaCalendario');
+function cambiarVista(vista) {
+    const vLista = document.getElementById('vistaLista');
+    const vCal = document.getElementById('vistaCalendario');
 
-        if (vista === 'lista') {
-            vLista.classList.remove('d-none');
-            vCal.classList.add('d-none');
-        } else {
-            vLista.classList.add('d-none');
-            vCal.classList.remove('d-none');
-        }
+    if (vista === 'lista') {
+        vLista.classList.remove('d-none');
+        vCal.classList.add('d-none');
+    } else {
+        vLista.classList.add('d-none');
+        vCal.classList.remove('d-none');
     }
+}
 
-    
+function renderLista() {
+    let html = `
+    <table class="table table-hover table-striped table-bordered align-middle">
+        <thead class="table-dark">
+            <tr><th>CÓDIGO</th><th>ASIGNATURA</th><th class="text-center">GRUPO</th><th class="text-center">DÍA</th><th class="text-center">HORARIO</th><th class="text-center">AULA</th></tr>
+        </thead>
+        <tbody>`;
+
+    materias.forEach(m => {
+        html += `<tr>
+            <td>${m.codigo}</td><td class="fw-bold">${m.nombre}</td>
+            <td class="text-center"><span class="badge bg-primary">${m.grupo}</span></td>
+            <td class="text-center">${m.dia}</td>
+            <td class="text-center">${m.horaInicio}:00 - ${m.horaFin - 1}:40</td>
+            <td class="text-center"><span class="badge bg-secondary">${m.aula}</span></td>
+        </tr>`;
+    });
+    appContainer.innerHTML = html + `</tbody></table>`;
+}
